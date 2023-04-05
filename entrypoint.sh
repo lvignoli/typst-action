72f02bd322076a535aa54d675b885d23c608abc8
Louis Vignoli <louis.vignoli@gmail.com>
2023-04-05 22:11:55 +0200

    Add support for multiple files

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,15 @@
 version=$(typst --version)
 echo "Using version $version"
 
-echo "Building $1"
-typst compile $1 main.pdf
+source_file="${1}"
+
+readarray -t source_file <<< "$source_file"
+
+for f in "${source_file[@]}"; do
+	if [[ -z "$f" ]]; then
+		continue
+	fi
+
+	echo "Building '$f'…"
+	typst compile "$f"
+done
