9c8493ddcb545c823e3eb180a9284645cf6e2c30
Louis Vignoli <louis.vignoli@gmail.com>
2023-05-01 16:55:13 +0200

    Refactor entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,19 @@
-import argparse
 import logging
 import subprocess
 import sys
+
+
+def compile(command: list[str]) -> bool:
+    logging.debug("Running: " + " ".join(command))
+
+    result = subprocess.run(command, capture_output=True, text=True)
+    try:
+        result.check_returncode()
+    except subprocess.CalledProcessError:
+        logging.error(f"Compiling {filename} failed with stderr: \n {result.stderr}")
+        return False
+
+    return True
 
 
 source_files = sys.argv[1].splitlines()
@@ -12,6 +24,8 @@ logging.basicConfig(level=logging.DEBUG)
 version = subprocess.run(["typst", "--version"], capture_output=True, text=True).stdout
 logging.info(f"Using version {version}")
 
+success: dict[str, bool] = {}
+
 for filename in source_files:
 
     filename = filename.strip()
@@ -21,10 +35,11 @@ for filename in source_files:
 
     logging.info(f"Building {filename}")
     command = ["typst"] + options + ["compile", filename]
-    logging.debug("Running: " + " ".join(command))
+    success[filename] = compile(command)
 
-    result = subprocess.run(command, capture_output=True, text=True)
-    try:
-        result.check_returncode()
-    except subprocess.CalledProcessError:
-        logging.error(f"Compiling {filename} failed with stderr: \n {result.stderr}")
+
+for filename, status in success.items():
+    logging.info(f"{filename}: {'✔' if status else '❌'}")
+
+if not all(success.values()):
+    sys.exit(1)
